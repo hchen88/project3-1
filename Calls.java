7316f46525a3064c283cc8a6f94be845a29a1a95
hchen88 <31971182+hchen88@users.noreply.github.com>
2018-03-10 16:01:35 -0800

    Update Calls.java

--- Calls.java
+++ Calls.java
@@ -2,16 +2,20 @@ package edu.project3;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Calls{
 	//Formats the call log with time stamp, name, number times called, incoming or outgoing.
 	
-	ArrayList<String> callLog = new ArrayList<>();
+	//private static ArrayList<String> callLog = new ArrayList<>(); // actual call log  , would be easier with Hash Map
+	
+	private static HashMap<String, String> callLogMap = new HashMap<>(); // key string- contact name , value toString();
+	private static HashMap<String, Integer> callOccurences = new HashMap<>(); //key contact( phone number or contact name), value numOccurances
 	private int outgoingCounter = 0;
 	private int incomingCounter = 0;
 	private String callType = "";
 	
-	
+
 	public void makeCallNumber(String number) {
 		
 		this.outgoingCounter ++;
@@ -21,14 +25,34 @@ public class Calls{
 	}
 	
 	
-	public void makeCallPreset() {
-
+	public void makeCallPreset(int presetNum) {
+		
+		
 		this.outgoingCounter ++;
 		String outgoingTimeStamp = timeStamp(); 
 		callType = "(Outgoing)";
-		//need to implement call log
+		//need to add element to ArrayList
+		String finalStr = "" + outgoingTimeStamp + callType;
+		callLog.add(finalStr);
 	}
 	
+	public void makeNonContactCall(String randomNumber) {
+		//needs conditonal if random call was called previously or not.
+		if(checkCallLog(randomNumber)) { //number was found
+			callOccurences.replace(randomNumber,callOccurences.get(randomNumber) + 1 ); //increments.
+			String outgoingTimeStamp = timeStamp(); //calls timeStamp method for current time
+			String callStr  = randomNumber + callOccurences.get(randomNumber) +"\n"+ outgoingTimeStamp + "(Outgoing)";
+			// String for call Line
+			callOccurences.replace(randomNumber, callStr); //replaces 
+		}else {
+			callOccurences.put(randomNumber, 1); // puts new randomnumber in with 1 occurance
+			String outgoingTimeStamp = timeStamp(); //calls timeStamp method for current time
+			String callStr = randomNumber + "\n"+ outgoingTimeStamp + "(Outgoing)"; // String for call Line
+			callOccurences.add(callStr); //appends call String variable to ArrayList	
+		}
+	
+		
+	}
 	
 	/**
 	 * this is a method that increments the incomingCounter
@@ -41,6 +65,10 @@ public class Calls{
 		String incomingTimeStamp = timeStamp(); 
 		callType = "(Incoming)";
 		//need to implement call log
+	}
+	
+	public ArrayList getCallLog () {
+		return callLog;
 	}
 	
 	
@@ -71,11 +99,23 @@ public class Calls{
 
 		
 	}
+	
+	public boolean checkCallLog(String contact) {
+		boolean found = false;
+		if (callLogMap.containsKey(contact)) {
+			found = true;
+		}
+		
+		return found;
+		
+	}
 
-	public String toString() {
+	public String toString() { //to string for calls 
 		
 		String str = "";
+		
 		str = "\n";
+		
 		return str;
 		
 	}
