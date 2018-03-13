db585dcadc753b1b5b6da588b76058e0d65e5189
areema578 <30280730+areema578@users.noreply.github.com>
2018-03-13 00:28:10 -0700

    Update PresetList.java

--- PresetList.java
+++ PresetList.java
@@ -1,50 +1,46 @@
 package edu.project3;
 
 public class PresetList {
-	//Adds, delete, rearrange, and displays preset list
-	//String[] presetStringList = new String[5];
-	//Contact[] presetContactList = new Contact[5]; 
 	String copy = "";
-	Contact copyObj = new Contact();
-	
 	private static Contact[] presetContactList = new Contact[5];
 	private static String[] presetStringList = new String[5];
 	
+	/**
+	 * makes preset list object
+	 */
 	public PresetList() {
 		
 	}
 	
 	/**
-	 * 
-	 * @param presetNum
-	 * @param contactObj
+	 * adds preset to the array
+	 * @param presetNum - preset number
+	 * @param contactObj - contact object
 	 */
-	public void addPreset(int presetNum, Contact contactObj) {
-		
+	public static void addPreset(int presetNum, Contact contactObj) {
 		presetStringList[presetNum - 1 ] =  contactObj.getName();
 		presetContactList[presetNum-1] = contactObj;
-		
 	}
 	
 	/**
-	 * 
-	 * @param presetNum
+	 * deletes preset in the array
+	 * @param presetNum - preset number
 	 */
-	public void deletePreset(int presetNum) {
+	public static void deletePreset(int presetNum) {
 		presetStringList[presetNum - 1] =  null;
 		presetContactList[presetNum-1] = null;
-		
 	}
 	
 	/**
-	 * 
-	 * @param presetNum1
-	 * @param presetNum2
+	 * rearranges preset
+	 * @param presetNum1 - first preset number
+	 * @param presetNum2 - second preset number
 	 */
 	
-	public void rearrangePreset(int presetNum1, int presetNum2) {
-		copy = presetStringList[presetNum1 - 1];
-		copyObj = presetContactList[presetNum1 - 1];
+	public static void rearrangePreset(int presetNum1, int presetNum2) {
+		
+		String copy = presetStringList[presetNum1 - 1];
+		Contact copyObj = presetContactList[presetNum1 - 1];
 		presetStringList[presetNum1 -1] = presetStringList[presetNum2 - 1];
 		presetContactList[presetNum1 -1] = presetContactList[presetNum2 - 1];
 		presetStringList[presetNum2 - 1] = copy;
@@ -53,15 +49,22 @@ public class PresetList {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * gets the preset list
+	 * @return - the preset list
 	 */
-	public String[] getPresetContactList() {
-		return presetStringList;
+	public static Contact[] getPresetContactList() {
+		return presetContactList;
 	}
-		
+	
+	/**
+	 * Formats the preset list
+	 */
 	public String toString() {
 		String str = "";
+		
+		for (int i = 0; i < 5; i++) {
+			str +=  Integer.toString(i+1 ) + ". " + presetStringList[i] + "\n";
+		}
 		for (String s : presetStringList) {
 			str += s;
 			str += "\n";
@@ -69,6 +72,4 @@ public class PresetList {
 		
 		return str;
 	}
-	
-
 }
