e04a6302d3c27c8f6db7c209257c6632e4fd8754
areema578 <30280730+areema578@users.noreply.github.com>
2018-03-13 00:25:33 -0700

    Update CellphoneTester.java

--- CellphoneTester.java
+++ CellphoneTester.java
@@ -1,10 +1,9 @@
 package edu.project3;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.TreeMap;
+import edu.project3.PhoneBook;
 
 public class CellphoneTester {
 
@@ -19,58 +18,84 @@ public class CellphoneTester {
 		Scanner in = new Scanner(System.in);
 		String userInput; 
 		PhoneBook phoneBook = new PhoneBook();
-		PresetList presetList = new PresetList();
-		CallLog callLog = new CallLog();
+		//PresetList presetList = new PresetList();
+		String[] presetList = new String[5];
+		HashMap<String, Calls>  callLog = Calls.getCallLog();
+		TreeMap<String, Contact> phoneBookMap = PhoneBook.getPhoneBook();
+		HashMap<String, FavoriteContactFrame> contactFrameMap = new HashMap<>();
+		int fWidth = 900;
+		int fHeight = 600;
 		
-		//TreeMap<String, Contact> phoneBook = new TreeMap<String, Contact>();
-		//TreeMap<String, Contact> numberBook = new TreeMap<String, Contact>();
-	
 		do {
 			System.out.println(mainMenu()); 
-			userInput = in.nextLine().trim(); 
+			userInput = in.nextLine().trim();
+			callLog = Calls.getCallLog();
+			phoneBookMap = PhoneBook.getPhoneBook();
+			
 			if (userInput.equals("1")) {
-				//receives a call
-				
-			}
-			else if (userInput.equals("2")) {
+				if (!callLog.containsKey("5555555555")) { //no received calls in call log yet
+					callLog.put("5555555555", new Calls("Incoming", "5555555555", false)); //adds new call to call log
+					System.out.println("Received call from (555)555-5555");
+				} else {
+					callLog.get("5555555555").updateCallLog("5555555555"); 
+					}
+							
+			}else if (userInput.equals("2")) {
 				System.out.println("Choose an option: \n1. By using phone number. \n2. By using contact. \n3. By using preset favorite.");
 				String callInput = in.nextLine().trim();
 				if (callInput.equals("1")) { //makes call by using phone number
 					System.out.println("Enter a ten digit phone number:");
 					String phoneNumber = in.nextLine().trim();
-					PhoneBook phoneBookObj = new PhoneBook(); //instantiates PhoneBook Object
-					TreeMap<String, Contact> phoneBookMap = phoneBookObj.getphoneBookMap();  // calls phoneBookMap and assigns it to local variable
-					for(Map.Entry<String,Contact> entry : phoneBookMap.entrySet()) {
-						  String key = entry.getKey();
-						  Contact value = entry.getValue(); // value of object type
-						  if ( phoneNumber.equals(value.getNum())) { //checks if number entered is already an existing contact
-							  value.makeCall();
-							  value.setTimeStamp();
-							  System.out.println("Making call to" + value.getName()); //makes call to the contact.
-							  														 // need to implement a case where phone number is not in phonebook.
+					
+					if(!phoneBookMap.containsKey(phoneNumber)) { //is not in phone book
+						if(!callLog.containsKey(phoneNumber)) { //is not in phone book and not call log
+							callLog.put(phoneNumber, new Calls("Outgoing",phoneNumber, false)); //adds new call to call log
+							System.out.println("Making call to " + phoneNumber);
+							
+						}else {   //is not in  phone book but is in call log
+							callLog.get(phoneNumber).updateCallLog(phoneNumber); //adds call to call log.
+							System.out.println("Making call to" + phoneNumber);
 						}
+					}else { //is in phone book 
+						if(!callLog.containsKey(phoneNumber)) { //is in phone book but not in callLog yet
+							String contactName = phoneBookMap.get(phoneNumber).getName();
+							callLog.put(phoneNumber, new Calls("Outgoing", contactName, true)); //adds new call to call log
+							System.out.println("Making call to" + contactName);
+						}else {  // is in phone book but already in call log.
+							callLog.get(phoneNumber).updateCallLog(phoneNumber); //adds call to call log.
+							String contactName = phoneBookMap.get(phoneNumber).getName();
+							System.out.println("Making call to" + contactName);  //done
+						}	
 					}
-				}
-				else if (callInput.equals("2")) { //makes call by using contact
-					String option;
+					
+				}else if (callInput.equals("2")) { //makes call by using contact 
+					String option = null;
 					do { 
+						callLog = Calls.getCallLog();
+						phoneBookMap = PhoneBook.getPhoneBook();
 						System.out.println("Enter a contact name:");
 						String contactName = in.nextLine().trim();
-						boolean found = phoneBook.checkPhonebook(contactName);
-						if (found) {
-							//print contact
-							Contact receivedContact = phoneBook.getContact(contactName);
-							System.out.println(receivedContact);
+						boolean found = PhoneBook.checkPhonebook(contactName);
+						if (found) { //in Phone book
+							System.out.println(phoneBookMap.get(contactName)); //print contact info object
 							System.out.println("Would you like to call " + contactName + "?\n1. Yes \n2. No");
 							String call = in.nextLine().trim();
-							if (call.equals("1")) { 
-								receivedContact.makeCall();
-								receivedContact.setTimeStamp();
-								System.out.println("Making call to " +  contactName);
+							if (call.equals("1")) { //calling by contactName 
+								if(!callLog.containsKey(contactName)){ //call log does not contain contactName yet		
+									callLog.put(contactName, new Calls("Outgoing", contactName, true));
+									System.out.println("Making call to " +  contactName);
+									option = "2";
+								}else { //contact already in log
+									callLog.get(contactName).updateCallLog(contactName);
+									System.out.println("Making call to " +  contactName);
+									option = "2";
+								}
+							}else {
+								option = "2";//does nothing to break out of loop
+								
 							}
-							option = "2"; 
-						} 
-						else {
+						}else{ //not found in phone book
+							System.out.println("Name was not in PhoneBook!");
 							System.out.println("Select an option: \n1. Enter a name again. \n2. Go to main menu.");
 							option = in.nextLine().trim();
 						}
@@ -78,9 +103,25 @@ public class CellphoneTester {
 				}
 				else if(callInput.equals("3")) { //makes call by using preset favorites
 					// CALLS USING PRESETS
-					System.out.println("Enter a preset number:");
-					int presetNum = in.nextInt();
+					//Working here
+					int presetNum = 5;
+					callLog = Calls.getCallLog();
+					phoneBookMap = PhoneBook.getPhoneBook();
 					
+					do {
+						System.out.println("Enter a preset number:");
+						presetNum = in.nextInt();
+						Contact[] contact = PresetList.getPresetContactList();
+						Contact contactObj = contact[presetNum-1];
+						String contactName = contactObj.getName();
+						String contactNum = contactObj.getNum();
+						System.out.println("Calling" + contactName + "\t" + contactNum);
+						if ( callLog.containsKey(contactName)) { //in callog already
+							callLog.get(contactName).updateCallLog(contactName);
+						} else { // not in call log
+							callLog.put(contactName, new Calls("Outgoing", contactName, true));
+						}
+					} while(presetNum < 6 && presetNum > 0 );
 				}
 			}
 			else if (userInput.equals("3")) {
@@ -99,11 +140,13 @@ public class CellphoneTester {
 					System.out.println("New contact added.");
 				}
 				else if (option.equals("2")) {
+					System.out.println("Your Phonebook:\n");
+					System.out.println(phoneBook);
 					System.out.println("Enter the name of the contact to be editted:");
 					String contactName = in.nextLine().trim();
-					boolean found = phoneBook.checkPhonebook(contactName);
+					boolean found = PhoneBook.checkPhonebook(contactName);
 					if (found) {
-						Contact receivedContact = phoneBook.getContact(contactName);
+						Contact receivedContact = PhoneBook.getContact(contactName);
 						System.out.println("Enter a new name for the contact:");
 						String newName = in.nextLine().trim();
 						receivedContact.editName(newName);
@@ -125,7 +168,7 @@ public class CellphoneTester {
 				else if (option.equals("3")) {
 					System.out.println("Enter the name of the contact to be deleted:");
 					String name = in.nextLine().trim();
-					boolean found = phoneBook.checkPhonebook(name);
+					boolean found = PhoneBook.checkPhonebook(name);
 					if (found) {
 						phoneBook.delContact(name);
 						System.out.println("Contact deleted.");
@@ -137,9 +180,9 @@ public class CellphoneTester {
 			else if (option.equals("4")) {
 					System.out.println("Enter contact name to display:");
 					String contactName = in.nextLine().trim();
-					boolean found = phoneBook.checkPhonebook(contactName);
+					boolean found = PhoneBook.checkPhonebook(contactName);
 					if (found) {
-						Contact receivedContact = phoneBook.getContact(contactName);
+						Contact receivedContact = PhoneBook.getContact(contactName);
 						System.out.println(receivedContact);
 					}else {
 						System.out.println("Contact doesn't exist.");
@@ -147,7 +190,6 @@ public class CellphoneTester {
 				}
 			}
 			else if (userInput.equals("4")) {
-				// PRINT PHONEBOOK TREEMAP
 				System.out.println("Your Phonebook:\n");
 				System.out.println(phoneBook);
 			}
@@ -163,13 +205,19 @@ public class CellphoneTester {
 					//ADD PRESET
 					System.out.println("Enter contact you would like to preset: ");
 					String contact = in.nextLine().trim();
-					boolean found = phoneBook.checkPhonebook(contact);
+					boolean found = PhoneBook.checkPhonebook(contact);
 					if(found) {
 						System.out.println("Enter preset number (1-5): ");
-						int presetNum = in.nextInt();
-						Contact receivedContact = phoneBook.getContact(contact);
-						presetList.addPreset(presetNum, receivedContact);
-						System.out.println("Preset added.");
+						String presetNum = in.nextLine().trim();
+						int intPresetNum = Integer.parseInt(presetNum);
+						PresetList.addPreset(intPresetNum, PhoneBook.getContact(contact));
+						String formatedNum ="(" + PhoneBook.getContact(contact).getNum().substring(0,3) + ")" + PhoneBook.getContact(contact).getNum().substring(3,6) + "-" + PhoneBook.getContact(contact).getNum().substring(6);
+						System.out.println("Go to C:\\Users\\Scott Arima\\Desktop\\Pokemon\\ to view photos");
+						System.out.println("Enter a picture you would like to use for this Preset:");
+						String pictureName = in.nextLine().trim();
+						contactFrameMap.put(contact, new FavoriteContactFrame("C:\\Users\\Scott Arima\\Desktop\\Pokemon\\" + pictureName +".jpg", fWidth, fHeight, contact, formatedNum, PhoneBook.getContact(contact).getEmail(), PhoneBook.getContact(contact).getNotes()));
+						System.out.println("Preset added with this picture");
+						contactFrameMap.get(contact).displayContactFrame();
 					}else {
 						System.out.println("Contact doesn't exist in the phonebook.");
 					}
@@ -177,7 +225,7 @@ public class CellphoneTester {
 					//DELETE PRESET
 					System.out.println("Enter preset number you would like to delete:");
 					int presetNum = in.nextInt();
-					presetList.deletePreset(presetNum);
+					PresetList.deletePreset(presetNum);
 					System.out.println("Preset deleted");
 				}else if(option.equals("4")) {
 					//REARRANGE PRESETS
@@ -186,15 +234,17 @@ public class CellphoneTester {
 					int presetNum1 = in.nextInt();
 					System.out.println("Enter second preset number:");
 					int presetNum2 = in.nextInt();
-					presetList.rearrangePreset(presetNum1, presetNum2);
+					PresetList.rearrangePreset(presetNum1, presetNum2);
 					System.out.println("Presets rearranged.");
 				}
 			}
 			else if (userInput.equals("6")) {
-				// DISPLAY CALL LOG
-				
-					
-				
+				System.out.println(Calls.getShortCalls());
+				System.out.println("Would you like to view detailed calls?Y/N");
+				String response = in.nextLine();
+				if(response.equals("Y")) {
+				System.out.println(Calls.getDetailedCalls());
+				}
 			}
 		}while (!userInput.equals("7")); 
 		System.out.println("Goodbye");
