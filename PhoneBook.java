6df787327c86620fe67f4cdf9c8d3a89ac6c9cef
areema578 <30280730+areema578@users.noreply.github.com>
2018-03-13 00:27:39 -0700

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,6 +1,5 @@
 package edu.project3;
 
-import java.util.Set;
 import java.util.TreeMap;
 
 public class PhoneBook {
@@ -9,25 +8,41 @@ public class PhoneBook {
 	 * Stores all contacts
 Adds/Deletes contact
 Displays contacts in alphabetical order
-
 	 */
+	 
+	private static TreeMap<String, Contact> phoneBookMap = new TreeMap<String, Contact>(); //key contact name or number , value - Contact obj
 	
-	private static TreeMap<String, Contact> phoneBookMap = new TreeMap<String, Contact>();
+	/**
+	 * gets the phonebook tree map
+	 * @return phoneBookMap - phonebook tree map
+	 */
+	public static TreeMap<String,Contact> getPhoneBook() {
+		return phoneBookMap;
+	}
 	
+	/**
+	 * makes phonebook object
+	 */
 	public PhoneBook() {
 		
 	}
 	/**
 	 * This method is used to get phoneBookMap
-	 * @returns phoneBookmap
+	 * @returns phoneBookmap - phonebook tree map
 	 */
 	public TreeMap<String, Contact> getphoneBookMap() {
 		return phoneBookMap;
 	}
 	
-	public Contact getContact(String contactName) {
+	/**
+	 * Gets the contact in the phonebook tree map
+	 * @param contactName - contact name as a string
+	 * @return phoneBookMap - phonebook tree map
+	 */
+	public static Contact getContact(String contactName) {
 		return phoneBookMap.get(contactName);
 	}
+	
 
 	/**
 	 * This method adds a new element to phoneBookMap with the key as a 
@@ -35,9 +50,9 @@ Displays contacts in alphabetical order
 	 * @param contactName- String variable for each contact
 	 * @param contactObj- Object for each contact
 	 ***/
-	public void addContact(String contactName, Contact contactObj   ) {
-		if ( !phoneBookMap.containsKey(contactName)) {
-			phoneBookMap.put(contactName, contactObj);
+	public void addContact(String phoneNumber, Contact contactObj   ) {
+		if ( !phoneBookMap.containsKey(phoneNumber)) {
+			phoneBookMap.put(phoneNumber, contactObj);
 		} else {
 			System.out.println("Contact already added!");
 		}
@@ -55,8 +70,12 @@ Displays contacts in alphabetical order
 		
 	}
 	
-	
-	public boolean checkPhonebook(String contactName) {
+	/**
+	 * Checks the phonebook to see if the contact is present
+	 * @param contactName - contact name as a string
+	 * @return found - true or false if found
+	 */
+	public static boolean checkPhonebook(String contactName) {
 		
 		boolean found = false;
 		for( String key : phoneBookMap.keySet()) {
@@ -66,19 +85,17 @@ Displays contacts in alphabetical order
 		}
 		return found;
 	}
-	
+	 
 	/**
-	 * This is a method 
+	 * Formats the phonebook tree map 
 	 */
 	public String toString() { //all contact information
 		String str = "";
-		Set<String> keys = phoneBookMap.keySet();
-        for(String key: keys){
-            System.out.println(phoneBookMap.get(key));
+        for(Contact c : phoneBookMap.values()){
+        	String numFormat = "(" + c.getNum().substring(0,3) + ")" + c.getNum().substring(3,6) + "-" + c.getNum().substring(6); 
+            str += c.getName() + "\t" + numFormat + "\t" + c.getEmail() + "\t" + c.getNotes() + "\n";
+
         }
 	    return str;
 	}
-	
-	
 }
-
